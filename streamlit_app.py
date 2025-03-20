05e469ea52877fd79433ca5503fe01d3da9b009d
mzctxtest <michael.ziemann@cenitex.vic.gov.au>
2025-03-20 14:48:16 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        import requests
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
@@ -49,4 +50,3 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success("Your Smoothie is ordered!", icon="✅") 
    
-import requests
