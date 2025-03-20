ecdd3dd2eaa717e6c2f957460d4c4ebb0aaab522
mzctxtest <michael.ziemann@cenitex.vic.gov.au>
2025-03-20 14:38:58 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if time_to_insert:
     st.success("Your Smoothie is ordered!", icon="✅") 
    
 import requests
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json()) 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json()) 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
